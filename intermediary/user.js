371d17b11edba9fb31e677b930afd4a228c4d77d
fannyblixt <fanny-blixt@hotmail.com>
2019-11-21 12:23:57 +0100

    web

--- intermediary/user.js
+++ intermediary/user.js
@@ -9,10 +9,10 @@ function getUserFirstname(user_id, callback){
     if (this.readyState == 4 && this.status == 200) {
            // Typical action to be performed when the document is ready:
            //alert(xhttp.responseText);
-      var arr = JSON.parse(xhttp.responseText);
+      var arr = xhttp.responseText;
       
 
-      if (arr != null) {
+      if (arr.length > 0) {
           callback(null, arr);  
       }
     }
