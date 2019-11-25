200721bccd43f2a41a84dc181910e072537ac10f
saraandersson <saraandersson>
2019-11-25 15:25:24 +0100

    loginsida

--- intermediary/user.js
+++ intermediary/user.js
@@ -20,7 +20,31 @@ function getUserFirstname(user_id, callback){
   
 
   xhttp.send(JSON.stringify(data));
-  
+
+}
+
+function checkLogin(email, password){
+  var xhttp = new XMLHttpRequest();
+  var email_val = email;
+  var password_val = password;
+  xhttp.open("POST", "/loginUser", true);
+  xhttp.setRequestHeader('Content-Type', 'application/json');
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+           // Typical action to be performed when the document is ready:
+           //alert(xhttp.responseText);
+      var arr = JSON.parse(xhttp.responseText);
+      console.log(arr);
+
+      if (arr==1){
+        window.location.replace("../website/index_customer");
+      } 
+      
+    }
+  }
+
+  var data = {email: email_val, password: password_val};
   
 
+  xhttp.send(JSON.stringify(data));
 }
