0da132b4af2623794ed15eccd84cd7cf39c059c1
fannyblixt <fanny-blixt@hotmail.com>
2019-11-25 16:06:47 +0100

    test

--- server.js
+++ server.js
@@ -106,9 +106,9 @@ app.post('/loginUser',function(req,res){
           if(result.rowCount == "1" && (result.rows[0].role_id == 0||result.rows[0].role_id == 1)){
             req.session.role_id = result.rows[0].role_id;
             req.session.Users = result.rows[0].id; 
-            send_(err, "1", res);
+            send_(err, 1, res);
           }else{
-            send_(err,"Användarnamn eller lösenord är felaktigt", res);
+            send_(err, 0, res);
           }
         }
       })
