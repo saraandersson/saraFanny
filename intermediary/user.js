845093689687b7361382f98a244029d04582d064
saraandersson <saraandersson>
2019-11-26 13:40:14 +0100

    profil för customer fixat

--- intermediary/user.js
+++ intermediary/user.js
@@ -139,3 +139,22 @@ function editProfile(){
   xhttp.send(JSON.stringify(data));
 
 }
+
+function getUser(id){
+  var xhttp = new XMLHttpRequest();
+  var id_val = id;
+  xhttp.open("POST", "/getEnergy", true);
+  xhttp.setRequestHeader('Content-Type', 'application/json');
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+           // Typical action to be performed when the document is ready:
+           //alert(xhttp.responseText);
+      var arr = JSON.parse(xhttp.responseText);
+      callback(null, arr); 
+      
+  }
+}
+    var data = {id: id_val};
+  
+    xhttp.send(JSON.stringify(data));
+}
