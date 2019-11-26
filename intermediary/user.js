e7a4d7a229986f0e84c30c1e074ff8034af25550
saraandersson <saraandersson>
2019-11-26 09:39:32 +0100

    registrationsida fixad

--- intermediary/user.js
+++ intermediary/user.js
@@ -46,7 +46,38 @@ function checkLogin(email, password){
 
   var data = {email: email_val, password: password_val};
   
-  window.alert(data);
+
+  xhttp.send(JSON.stringify(data));
+}
+
+
+function createUser(firstname, lastname, email, password, area, consumption){
+  var xhttp = new XMLHttpRequest();
+  var firstname_val = firstname;
+  var lastname_val = lastname;
+  var email_val = email;
+  var password_val = password;
+  var area_val = area;
+  var consumption_val = consumption;
+  xhttp.open("POST", "/createUser", true);
+  xhttp.setRequestHeader('Content-Type', 'application/json');
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+           // Typical action to be performed when the document is ready:
+           //alert(xhttp.responseText);
+      var arr = JSON.parse(xhttp.responseText);
+      window.alert(arr);
+
+      if (arr.size > 0){
+        window.location.replace("../website/index_customer.html");
+      } 
+      
+    }
+  }
+
+
+  var data = {firstname: firstname_val, lastname: lastname_val, email: email_val, password: password_val, area: area_val, consumption: consumption_val};
+  
 
   xhttp.send(JSON.stringify(data));
 }
