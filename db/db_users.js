f70d9c5c1d7dafb8cd5d15cf846f2472d02f6511
saraandersson <saraandersson>
2019-12-10 15:11:16 +0100

    pop-up form till block prosumer

--- db/db_users.js
+++ db/db_users.js
@@ -352,14 +352,14 @@ blockUser(id, block, time){
 				console.log("User blocked uppdaterat");
 			}
 		});
-	var update = 'CREATE EVENT myevent ON SCHEDULE AT CURRENT_TIMESTAMP + INTERVAL ? SECOND DO UPDATE blocked SET blocked = ?;'
+	/*var update = 'CREATE EVENT myevent ON SCHEDULE AT CURRENT_TIMESTAMP + INTERVAL ? SECOND DO UPDATE blocked SET blocked = ?;'
 		con.query(update, [time,0], function(err, res){
 			if(err){
 				console.log(err);	
 			}else{
 				console.log("User unblocked testing");
 			}
-		});
+		});*/
 
 }
 
