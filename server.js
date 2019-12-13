c8ec754d07c6f2adfcd6ea817636615507ab13eb
fannyblixt <fanny-blixt@hotmail.com>
2019-12-13 09:12:50 +0100

    hej

--- server.js
+++ server.js
@@ -330,7 +330,7 @@ async function simulatorCall(idarr, fn){
 
   }
     //Get info about all prosumers production
-  db_user.getAllProsumersProduction((er,re){
+  db_user.getAllProsumersProduction((er,re) =>{
     fn(re);
   });
 
