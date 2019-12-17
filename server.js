5e84205a68a47370dd574d45110e89d6607677ea
fannyblixt <fanny-blixt@hotmail.com>
2019-12-17 12:57:58 +0100

    hej

--- server.js
+++ server.js
@@ -186,6 +186,21 @@ app.route('/login')
       });
 
 
+    app.route('/*')
+    .get((req,res)=>{
+     if (req.session.Users && req.cookies.user_id && req.session.role_id == 0) {
+          res.redirect('/website/index_customer.html');
+      }
+      else if(req.session.Users && req.cookies.user_id && req.session.role_id == 1){
+          res.redirect('/website/index_admin.html');
+        } 
+      else {
+           res.redirect('/website/index.html');
+        }
+      });
+
+
+
 
         
 
