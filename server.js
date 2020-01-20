65c25d59ff09a04e1822275303277fbcd856e625
saraandersson <saraandersson>
2020-01-20 19:35:08 +0100

    bildfix

--- server.js
+++ server.js
@@ -485,7 +485,8 @@ const upload = multer({
             console.log("Kommer till FAIL");
             //document.getElementById("errorMsg").innerHTML += '<br>Email already exist!';
             //window.alert('Email already exist!');
-            send_(err, result, res);
+            //send_(err, result, res);
+            res.render(body);
           }
             
             }); 
