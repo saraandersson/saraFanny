ce067f3fdc5a1c43c613b85e4297113502329222
saraandersson <saraandersson>
2020-01-20 15:12:48 +0100

    bildfix

--- server.js
+++ server.js
@@ -416,7 +416,12 @@ app.post('/createUser', function(req,res){
                   if(err){
                   console.log(err);
                   }
+                if(req.file){
                 db_user.addUser(0, 1 , req.body.firstname, req.body.lastname, req.body.email, hash, req.file.filename, req.body.area, 0, req.body.consumption);
+                }
+                else{
+                  db_user.addUser(0, 1 , req.body.firstname, req.body.lastname, req.body.email, hash, "", req.body.area, 0, req.body.consumption);
+                }
                 db_user.getUserId(req.body.email, hash, (error, results) =>{
                 db_user.addBlocked(results[0].id, 0, 0);
                 db_user.addSellBuy(results[0].id, 0.5 , 0.5);
@@ -432,7 +437,8 @@ app.post('/createUser', function(req,res){
               
           }else{
             console.log("Kommer till FAIL");
-            send_(err, result, res);
+            document.getElementById("errorMsg").innerHTML += '<br>Email already exist!';
+            //send_(err, result, res);
           }
         }
     })
